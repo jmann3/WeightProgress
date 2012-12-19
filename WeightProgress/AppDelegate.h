08b547bfca3addb8039fb6847e1eebeade1004be
Jack Mann <jrmann120@gmail.com>
2012-12-19 10:43:23 -0500

    Initial Commit

---
+++ WeightProgress/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeightProgress
+//
+//  Created by John Mann on 12/19/12.
+//  Copyright (c) 2012 John Mann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeightProgress/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WeightProgress
+//
+//  Created by John Mann on 12/19/12.
+//  Copyright (c) 2012 John Mann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
